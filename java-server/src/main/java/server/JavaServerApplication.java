704eea16c63fe5105c71d04072abe40e3bf9b64f
ggaydarov <georgidgaydarov@gmail.com>
2020-07-22 10:23:11 +0300

    Initial setup spring boot

---
+++ java-server/src/main/java/server/JavaServerApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.server")
+public class JavaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaServerApplication.class, args);
+    }
+
+}
