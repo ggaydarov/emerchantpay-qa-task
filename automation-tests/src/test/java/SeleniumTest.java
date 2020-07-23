261831ff20e46ecc4b98720f997876c5732c5c11
ggaydarov <georgidgaydarov@gmail.com>
2020-07-23 20:33:19 +0300

    Added simple automation java class

---
+++ automation-tests/src/test/java/SeleniumTest.java
@@ -0,0 +1,11 @@
+import io.github.bonigarcia.seljup.SeleniumExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@ExtendWith(SeleniumExtension.class)
+class SeleniumTest {
+
+    @Test
+    void projectIsConfigured(ChromeDriver driver) {}
+}
